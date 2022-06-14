24d8b88392129e40cf670be2dff55f0cf7401ab0
owentr1369 <tamvaa1306@gmail.com>
2022-06-14 10:11:20 +0700

    add fontawesome  and materialidesignicon

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,18 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
-
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  icons: {
+    iconfont: "md" || "fa",
+  },
+  theme: {
+    themes: {
+      dark: {
+        background: "#000000",
+      },
+    },
+  },
 });
